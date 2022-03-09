eb7c746c0ac0dd8b295c006a4abd780632f17060
Zumrad Belyakova <s.zumrad@gmail.com>
2022-03-09 12:26:17 +0300

    Task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,12 @@
 // Напиши скрипт который, при наборе текста в инпуте
 // input#name - input(событие input), подставляет его текущее значение
-// в span#name - output. 
-const inputName = document.querySelector('#name-input');
-const outputName = document.querySelector('#name-output');
+// в span#name - output.
+const inputName = document.querySelector("#name-input");
+const outputName = document.querySelector("#name-output");
 // Если инпут пустой, в спане должна отображаться
 // строка "Anonymous".
-
+inputName.addEventListener("input", (event) => {
+  event.currentTarget.value === ""
+    ? (outputName.textContent = "Anonymous")
+    : (outputName.textContent = event.currentTarget.value);
+});
